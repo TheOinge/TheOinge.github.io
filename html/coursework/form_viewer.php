aec7428ab276961f3dca62d4daf3ba633d7d23a9
Logan <ltrud78@gmail.com>
2023-09-28 21:54:48 -0600

    php fix?

--- html/coursework/form_viewer.php
+++ html/coursework/form_viewer.php
@@ -21,17 +21,21 @@
       <th>Value(s)</th>
     </tr>
     <?php
-      foreach ($_POST as $key => $value) {
-        echo "<tr>";
-        echo "<td>$key</td>";
-        echo "<td>";
-        if (is_array($value)) {
-          echo implode(", ", $value);
-        } else {
-          echo $value;
+      if ($_SERVER['REQUEST_METHOD'] === 'POST' || $_SERVER['REQUEST_METHOD'] === 'GET') { // Check the HTTP method
+        foreach ($_REQUEST as $key => $value) { // Use $_REQUEST to handle both GET and POST
+          echo "<tr>";
+          echo "<td>$key</td>";
+          echo "<td>";
+          if (is_array($value)) {
+            echo implode(", ", $value);
+          } else {
+            echo $value;
+          }
+          echo "</td>";
+          echo "</tr>";
         }
-        echo "</td>";
-        echo "</tr>";
+      } else {
+        echo "<tr><td colspan='2'>Invalid HTTP method</td></tr>";
       }
     ?>
   </table>
