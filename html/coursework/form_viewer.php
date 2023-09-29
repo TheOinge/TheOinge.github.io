e517f720d719c4c2e4e1e419f1e608a14cf6fc8a
Logan <ltrud78@gmail.com>
2023-09-28 22:25:06 -0600

    php fix 2

--- html/coursework/form_viewer.php
+++ html/coursework/form_viewer.php
@@ -1,43 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Form Viewer</title>
-  <style>
-    table {
-      border-collapse: collapse;
-    }
-
-    table, th, td {
-      border: 1px solid black;
-      padding: 5px;
-    }
-  </style>
+    <title>Form Data Response</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-  <h1>Form Data Viewer</h1>
-  <table>
-    <tr>
-      <th>Field</th>
-      <th>Value(s)</th>
-    </tr>
-    <?php
-      if ($_SERVER['REQUEST_METHOD'] === 'POST' || $_SERVER['REQUEST_METHOD'] === 'GET') { // Check the HTTP method
-        foreach ($_REQUEST as $key => $value) { // Use $_REQUEST to handle both GET and POST
-          echo "<tr>";
-          echo "<td>$key</td>";
-          echo "<td>";
-          if (is_array($value)) {
-            echo implode(", ", $value);
-          } else {
-            echo $value;
-          }
-          echo "</td>";
-          echo "</tr>";
+    <h1>Form Data Response</h1>
+    <table>
+        <tr>
+            <th>Field</th>
+            <th>Value(s)</th>
+        </tr>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] === "POST" || $_SERVER["REQUEST_METHOD"] === "GET") {
+            foreach ($_REQUEST as $field => $value) {
+                echo "<tr>";
+                echo "<td>$field</td>";
+                if (is_array($value)) {
+                    echo "<td>" . implode(", ", $value) . "</td>";
+                } else {
+                    echo "<td>$value</td>";
+                }
+                echo "</tr>";
+            }
         }
-      } else {
-        echo "<tr><td colspan='2'>Invalid HTTP method</td></tr>";
-      }
-    ?>
-  </table>
+        ?>
+    </table>
 </body>
 </html>
