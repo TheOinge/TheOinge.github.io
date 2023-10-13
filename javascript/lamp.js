fe0c1996ea45d920abfb32835cefff28ae6a2eb3
Logan <ltrud78@gmail.com>
2023-10-12 23:58:22 -0600

    lamp-stack

--- javascript/lamp.js
+++ javascript/lamp.js
@@ -1,11 +1,61 @@
-const url = "https://randomuser.me/api"
+let url = "https://randomuser.me/api"
 const getBtn = document.getElementById("getBtn")
-const apiData = document.getElementById("adiData")
+const apiData = document.getElementById("apiData")
 
 getBtn.addEventListener("click", getUser)
 
 getUser()
 
 function getUser(){
-    apiData.innerHTML = "BONSOIR!"
+    fetch(url)
+    .then(decodeData)
+    .then(success, fail);
+}
+
+function decodeData(response){
+
+    if(response.ok) {
+        apiData.innerHTML = "response is " + response.status;
+        return (response.json())
+    } else {
+        throw response.status
+    }
+
+}
+
+function success(userData) {
+    const apiFirst = userData.results[0].name.first;
+    const apiLast = userData.results[0].name.last;
+    const apiCountry = userData.results[0].location.country;
+    const apiAge = userData.results[0].dob.age;
+    // console.log(apiFirst)
+    // console.log(apiLast)
+    // console.log(apiCountry)
+    // console.log(apiAge)
+
+    const apiFirstInput = document.getElementById("apiFirst");
+    apiFirstInput.value = apiFirst;
+
+    // Update the value of the existing input fields
+    const addBtn = document.getElementById("addBtn");
+    addBtn.value = "Add This One";
+
+    // Update the other parts of the displayed user data
+    apiData.innerHTML = `
+        <img class="user" src="${userData.results[0].picture.large}" alt="Random User">
+        <h2 class="user">Meet ${apiFirst} ${apiLast}</h2>
+    `;
+
+    // Update hidden form fields for last name, country, and age
+    document.querySelector("input[name='last']").value = apiLast;
+    document.querySelector("input[name='country']").value = apiCountry;
+    document.querySelector("input[name='age']").value = apiAge;
+}
+
+
+
+function fail(error) {
+    apiData.innerHTML = "Something went wrong with parsing JSON."
+    mdnCodes = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status"
+    apiData.innerHTML+= `<br>The problem: <a href=${mdnCodes}>${error} Error</a>`
 }
--- php/lamp.php
+++ php/lamp.php
@@ -3,9 +3,86 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add</title>
+    <link rel="stylesheet" href="../css/lamp.css">
 </head>
 <body>
-    
+    <h1>Added</h1>
+    <?php
+            //MSU
+            // $servername = "localhost";
+            // $username = "user58";
+            // $password = "58qktp";
+            // $dbname = "db58";
+
+            // ammps
+            $servername = "localhost";
+            $username = "root";
+            $password = "mysql";
+            $dbname = "db58";
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $firstname = $_POST['first'];
+                $lastname = $_POST['last'];
+                $country = $_POST['country'];
+                $age = $_POST['age'];
+
+                $sql = "INSERT INTO randuser (first, last, country, age) VALUES ('$firstname', '$lastname', '$country', '$age')";
+
+                if (mysqli_query($conn, $sql)) {
+                    echo "New record created successfully";
+                    echo "<h2>Added: $firstname $lastname</h2>";
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+                mysqli_close($conn);
+            }
+
+            //  // Display past submissions in a table
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                $sql = "SELECT first, last, country, age FROM randuser";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    echo '<table>';
+                    echo '<thead>';
+                    echo '<tr>';
+                    echo '<th>First Name</th>';
+                    echo '<th>Last Name</th>';
+                    echo '<th>Country</th>';
+                    echo '<th>Age</th>';
+                    echo '</tr>';
+                    echo '</thead>';
+                    echo '<tbody>';
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<tr>';
+                        echo '<td>' . $row['first'] . '</td>';
+                        echo '<td>' . $row['last'] . '</td>';
+                        echo '<td>' . $row['country'] . '</td>';
+                        echo '<td>' . $row['age'] . '</td>';
+                        echo '</tr>';
+                    }
+
+                    echo '</tbody>';
+                    echo '</table>';
+                } else {
+                    echo "No past submissions yet.";
+                }
+
+                mysqli_close($conn);
+        ?>
+
 </body>
 </html>
